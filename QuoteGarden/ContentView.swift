e2d467822bd4a1f9c266e5e0acfa8b8b0eb3b7c2
FranicevicNikola <nikola007f@gmail.com>
2020-10-03 07:35:39 +0200

    Initial Commit

---
+++ QuoteGarden/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  QuoteGarden
+//
+//  Created by Master Family on 03/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
