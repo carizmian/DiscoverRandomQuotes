c7b473b172901d18f3e3d1b4b7600f6f57b0fbd2
FranicevicNikola <nikola007f@gmail.com>
2020-10-05 06:48:34 +0200

    just made it simple, only random quotes

--- QuoteGarden/ContentView.swift
+++ QuoteGarden/ContentView.swift
@@ -11,40 +11,29 @@ import Foundation
 
 struct ContentView: View {
     
-    @State private var quotes: [Quote] = []
+    @State private var quote: Quote = Quote(id: "", quoteText: "Tap the random button", quoteAuthor: "Nikola Franičević")
     @State private var searchText = ""
     
+    #warning("napravi da jedan quote po jedan gledas i da je random i da ga mores lajkat i to je to")
+    #warning("Favorites using CoreData")
     
     var body: some View {
         VStack {
+                HStack {
+                    Text("'\(quote.quoteText)'")
+                        .italic()
+                        .bold()
+                    Text("~\(quote.quoteAuthor)")
+                        .foregroundColor(.gray)
+                        .font(.subheadline)
+                }.animation(.interpolatingSpring(mass: 1.9, stiffness: 2.2, damping: 2.2, initialVelocity: 5.2))
             
-            SearchBar(text: $searchText)
-                .onTapGesture {
-                    quoteGardenApi().searchAuthor(author: searchText) { (quotes) in
-                        self.quotes = quotes
-                    }
-                }
+            //  SearchBar(text: $searchText)
             
-            Button(action: { quoteGardenApi().searchAuthor(author: searchText) { (quotes) in
-                self.quotes = quotes
+            Button(action: { quoteGardenApi().getRandomQuote { (quote) in
+                self.quote = quote
             } }) {
-                Text("Search")
-            }
-            
-            
-            VStack {
-                List {
-                    ForEach(quotes.filter({ searchText.isEmpty ? true : $0.quoteAuthor.contains(searchText) }), id: \.id) { quote in
-                        HStack {
-                            Text("'\(quote.quoteText)'")
-                                .italic()
-                                .bold()
-                            Text(quote.quoteAuthor)
-                                .foregroundColor(.gray)
-                                .font(.subheadline)
-                        }
-                    }
-                }
+                Text("Random")
             }
         }
     }
--- QuoteGarden/Model/Quote.swift
+++ QuoteGarden/Model/Quote.swift
@@ -10,7 +10,7 @@ import Foundation
 // MARK: - Response
 struct Response: Decodable {
     let statusCode: Int
-    let quotes: [Quote]
+    let quote: Quote
 }
 
 // MARK: - Quote
@@ -25,15 +25,11 @@ struct Quote: Decodable {
 
 class quoteGardenApi {
     
-    @Published var page = 1
-    @Published var chunkSize = 1000
-    
-    
-    func getAllQuotes(completion: @escaping ([Quote]) -> Void) {
+    func getRandomQuote(completion: @escaping (Quote) -> Void) {
         
-        var semaphore = DispatchSemaphore (value: 0)
+        let semaphore = DispatchSemaphore (value: 0)
         
-        let url = URL(string: "https://quote-garden.herokuapp.com/api/v2/quotes?page=\(page)&limit=\(chunkSize)")
+        let url = URL(string: "https://quote-garden.herokuapp.com/api/v2/quotes/random")
         
         var request = URLRequest(url: url!, timeoutInterval: Double.infinity)
         request.httpMethod = "GET"
@@ -49,7 +45,7 @@ class quoteGardenApi {
                 return
             }
             DispatchQueue.main.async {
-                completion(response.quotes)
+                completion(response.quote)
             }
             
             
@@ -61,40 +57,5 @@ class quoteGardenApi {
         semaphore.wait()
     }
     
-    
-    func searchAuthor(author: String, completion: @escaping ([Quote]) -> Void) {
-        
-        var semaphore = DispatchSemaphore (value: 0)
-        
-        let url = URL(string: "https://quote-garden.herokuapp.com/api/v2/authors/\(author)?page=1&limit=10")
-        
-        var request = URLRequest(url: url!, timeoutInterval: Double.infinity)
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                print(String(describing: error))
-                return
-            }
-            
-            guard let response = try? JSONDecoder().decode(Response.self, from: data) else {
-                print(String(describing: error))
-                return
-            }
-            
-            
-            
-            DispatchQueue.main.async {
-                completion(response.quotes)
-            }
-            
-            
-            print(String(data: data, encoding: .utf8)!)
-            semaphore.signal()
-        }
-        
-        task.resume()
-        semaphore.wait()
-    }
     
 }
