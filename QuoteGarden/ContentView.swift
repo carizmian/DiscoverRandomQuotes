8694ab01b1ec6bc8c59c7f2fdb53b1d8316a805f
FranicevicNikola <nikola007f@gmail.com>
2020-10-10 06:27:24 +0200

    added nicer buttons

--- QuoteGarden/ContentView.swift
+++ QuoteGarden/ContentView.swift
@@ -15,103 +15,112 @@ struct ContentView: View {
     @FetchRequest(entity: QuoteCD.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \QuoteCD.quoteAuthor, ascending: true)]) var favoriteQuotes: FetchedResults<QuoteCD>
     
     @State private var quote: Quote = Quote(id: "", quoteText: "Tap the random button", quoteAuthor: "Nikola Franičević", quoteGenre: "knowledge")
+    @State private var addedToFavorites = false
     
-    @State private var change = false
-    
-    #warning("Widgets with one of your favorite quote")
+    #warning("Widgets with your favorite quote")
     
     
     var body: some View {
         
-    
-    TabView {
         
-        
-        VStack {
+        TabView {
             
             
-            QuoteView(quoteGenre: "\(quote.quoteGenre)", quoteText: "\(quote.quoteText)", quoteAuthor: "\(quote.quoteAuthor)")
-
-            HStack {
+            VStack {
                 
-                Button(action: { quoteGardenApi().getRandomQuote { (quote) in
-                    withAnimation(.easeInOut(duration: 1)) {
-                    change.toggle()
-                    }
-                    self.quote = quote
-                } }) {
-                    VStack {
-                        Text("New quote")
-                    }.padding()
-                    .border(Color.accentColor)
+                
+                QuoteView(quoteGenre: "\(quote.quoteGenre)", quoteText: "\(quote.quoteText)", quoteAuthor: "\(quote.quoteAuthor)")
+                
+                VStack {
                     
-                }.padding()
-                
-                
-                
-                
-                Button(action: { addToFavorites(_: self.quote.id, self.quote.quoteText, self.quote.quoteAuthor, self.quote.quoteGenre) }) {
-                    
-                    VStack {
-                        Text("Add to favorites")
+                    Button(action: { quoteGardenApi().getRandomQuote { (quote) in
+                        addedToFavorites = false
+                        self.quote = quote
+                    } }) {
+                        VStack {
+                            Text("New quote")
+                                .fontWeight(.bold)
+                                .font(.title3)
+                                .padding()
+                                .background(Color.accentColor)
+                                .foregroundColor(.white)
+                                .cornerRadius(40)
+                        }
                         
-                    }.padding()
-                    .border(Color.accentColor)
+                    }
+                    
+
+                    Button(action: { addToFavorites(_: self.quote.id, self.quote.quoteText, self.quote.quoteAuthor, self.quote.quoteGenre) }) {
+                        
+                        VStack {
+                            Text("Add to favorites")
+                                .fontWeight(.bold)
+                                .font(.title3)
+                                .padding()
+                                .background(Color.accentColor)
+                                .foregroundColor(.white)
+                                .cornerRadius(40)
+                                .opacity(addedToFavorites ? 0 : 1)
+                                .animation(.default)
+
+                        }
+                        
+                    }
                     
                 }
+                
+                
+            }.tabItem {
+                Image(systemName: "wand.and.stars")
+                Text("Random")
             }
             
             
-        }.tabItem {
-            Image(systemName: "wand.and.stars")
-            Text("Random")
-        }
-        
-        
-        NavigationView {
-            
-            List {
-                ForEach(favoriteQuotes, id: \.id) { favoriteQuote in
-                    NavigationLink(destination: QuoteDetailView(favoriteQuote: favoriteQuote)) {
-                        Text(favoriteQuote.quoteText ?? "No Favorite Quote Yet")
-                    }
-                }.onDelete(perform: removeQuote)
+            NavigationView {
+                
+                List {
+                    ForEach(favoriteQuotes, id: \.id) { favoriteQuote in
+                        NavigationLink(destination: QuoteDetailView(favoriteQuote: favoriteQuote)) {
+                            Text(favoriteQuote.quoteText ?? "No Favorite Quote Yet")
+                        }
+                    }.onDelete(perform: removeQuote)
+                    
+                    
+                }.navigationTitle(Text("Your Favorites"))
+                .navigationBarItems(trailing: EditButton())
                 
                 
-            }.navigationTitle(Text("Your Favorites"))
-            .navigationBarItems(trailing: EditButton())
+            }.tabItem {
+                Image(systemName: "star.fill")
+                Text("Favorites")
+            }
             
-            
-        }.tabItem {
-            Image(systemName: "star.fill")
-            Text("Favorites")
+        }
+    }
+    
+    func addToFavorites(_ id: String, _ text: String, _ author: String, _ genre: String) {
+        let favoriteQuote = QuoteCD(context: self.moc)
+        favoriteQuote.id = id
+        favoriteQuote.quoteText = text
+        favoriteQuote.quoteAuthor = author
+        favoriteQuote.quoteGenre = genre
+        
+        addedToFavorites = true
+        try? self.moc.save()
+    }
+    
+    func removeQuote(at offsets: IndexSet) {
+        for index in offsets {
+            let favoriteQuote = favoriteQuotes[index]
+            moc.delete(favoriteQuote)
         }
         
+        do {
+            try moc.save()
+        } catch  {
+            return
+        }
     }
-}
-
-func addToFavorites(_ id: String, _ text: String, _ author: String, _ genre: String) {
-    let favoriteQuote = QuoteCD(context: self.moc)
-    favoriteQuote.id = id
-    favoriteQuote.quoteText = text
-    favoriteQuote.quoteAuthor = author
-    favoriteQuote.quoteGenre = genre
-    
-    try? self.moc.save()
-}
-
-func removeQuote(at offsets: IndexSet) {
-    for index in offsets {
-        let favoriteQuote = favoriteQuotes[index]
-        moc.delete(favoriteQuote)
-    }
-    
-    do {
-        try moc.save()
-    } catch  {
-        return
-    }
-}
 }
 
 
@@ -121,5 +130,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
 
