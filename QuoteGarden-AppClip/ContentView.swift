02bc94b67cded67dcce20dda8b7e70a78919854a
FranicevicNikola <nikola007f@gmail.com>
2021-02-06 05:57:59 +0100

    -added app clip

---
+++ QuoteGarden-AppClip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuoteGarden-AppClip
+//
+//  Created by Master Family on 06/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuoteGarden-AppClip/QuoteGarden_AppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteGarden_AppClipApp.swift
+//  QuoteGarden-AppClip
+//
+//  Created by Master Family on 06/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuoteGarden_AppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
