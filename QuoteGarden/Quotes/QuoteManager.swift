b64a07af3f5740e83ce7da6d2737e11bce6c31a7
FranicevicNikola <nikola007f@gmail.com>
2021-08-21 07:49:35 +0200

    - mvvvm

---
+++ QuoteGarden/Quotes/QuoteManager.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class QuoteViewModel: ObservableObject {
+    @Published var quote = Quote(id: "", quoteText: "Tap here to generate a random quote", quoteAuthor: "Nikola Franičević", quoteGenre: "help")
+    
+    func changeQuote(_ quote: Quote) {
+        self.quote = quote
+        
+    }
+}
---
+++ QuoteGarden/Quotes/QuoteViewModel.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+class QuoteViewModel: ObservableObject {
+    @Published var quotes = [Quote]()
+    @Published var quote = Quote(id: "", quoteText: "Tap here to generate a random quote", quoteAuthor: "Nikola Franičević", quoteGenre: "help")
+    
+    
+    
+    func getRandomQuote() {
+        let randomPage = Int.random(in: 1..<2)
+        let randomQuote = Int.random(in: 0..<36335)
+        let url = Bundle.main.url(forResource: "quotes\(randomPage).json", withExtension: nil)
+        
+        URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            guard let response = try? JSONDecoder().decode(Response.self, from: data) else {
+                print(String(describing: error))
+                return
+            }
+            DispatchQueue.main.async {
+                self.quote = response.data[randomQuote]
+            }
+        }.resume()
+        
+    }
+    
+    func getRandomQuotes(withCompletionHandler completionHandler: @escaping ([Quote]) -> Void) {
+        let randomPage = Int.random(in: 1..<2)
+        var randomQuote = Int.random(in: 0..<36335)
+        let url = Bundle.main.url(forResource: "quotes\(randomPage).json", withExtension: nil)
+        
+        URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            guard let response = try? JSONDecoder().decode(Response.self, from: data) else {
+                print(String(describing: error))
+                return
+            }
+            DispatchQueue.main.async {
+                // Appends 500 quotes
+                for _ in 1...500 {
+                    self.quotes.append(response.data[randomQuote])
+                    randomQuote = Int.random(in: 0..<36335)
+                }
+                completionHandler(self.quotes)
+            }
+        }.resume()
+        
+    }
+    
+    func changeQuote(_ quote: Quote) {
+        self.quote = quote
+        
+    }
+}
